05bb7c1da46cc73498df7a0b795fe93d2a035e6c
Miguel Peixe <miguel.peixe@aparabolica.com.br>
2017-07-07 09:58:33 -0300

    send message to authenticated users

--- src/telegram/auth.js
+++ src/telegram/auth.js
@@ -1,12 +1,18 @@
 module.exports = function (app) {
 
   const service = app.service('authorize');
+  const bot = app.bot;
 
-  app.bot.onText(/\/start (.+)/, (msg, match) => {
+  bot.onText(/\/start (.+)/, (msg, match) => {
     const token = match[1];
+    const chatId = msg.chat.id;
     service.create({
       userId: msg.from.id,
       userKey: token
+    }).then(() => {
+      bot.sendMessage(chatId, 'You are authenticated.');
+    }).catch(() => {
+      bot.sendMessage(chatId, 'Something went wrong, please try again.');
     });
   });
 
