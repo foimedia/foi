41c382653d39bd075529ee22224e2b0aea74c49d
Miguel Peixe <miguel.peixe@aparabolica.com.br>
2017-07-21 10:33:41 -0300

    fix build scripts and update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,9 @@ echo "Updating permissions..."
 chown -Rf node:node /src /usr/local/lib/node_modules
 if [ "$NODE_ENV" == "production" ]; then
   echo "Bundling assets..."
-  su-exec node:node yarn run build
+  su-exec node:node yarn run build:main
+  echo "Bundling widget.js..."
+  su-exec node:node yarn run build:widget
 fi
 echo "Executing process..."
 exec su-exec node:node /sbin/tini -- "$@"
