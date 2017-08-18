0b01b95656642ffed6f5bf06a606325a62f0a1d0
Miguel Peixe <miguel.peixe@aparabolica.com.br>
2017-08-18 17:47:40 -0300

    Bugfix: rehydration must always be in context to allow client to subscribe after validation. Also normalize service events filtering.

--- src/services/posts/posts.filters.js
+++ src/services/posts/posts.filters.js
@@ -1,8 +1,7 @@
 module.exports = function (data, connection, hook) {
-  if(connection.chats && connection.chats.indexOf(data.chatId)) {
+  if(connection.chats && connection.chats.indexOf(data.chatId))
     return data;
-  }
-  if(connection.user && data.userId == connection.userId)
+  if(connection.user && data.userId == connection.user.id)
     return data;
   return false;
 };
--- src/services/users/users.filters.js
+++ src/services/users/users.filters.js
@@ -1,7 +1,7 @@
 module.exports = function (data, connection, hook) {
-  if(data.id == connection.payload.userId || connection.user.roles.indexOf('admin') !== -1) {
-    return data;
-  } else {
-    return false;
-  }
+  if(
+    data.id == connection.user.id ||
+    connection.user.roles.indexOf('admin') !== -1
+  ) return data;
+  return false;
 };
