69b328278d519ca04eea92e83b0ccd42a4d81419
Miguel Peixe <miguel.peixe@aparabolica.com.br>
2017-08-18 15:39:47 -0300

    fix context update (check for object type and replace the rest)

--- client/reducers/context.js
+++ client/reducers/context.js
@@ -44,10 +44,10 @@ export default function reducer (state = initialState, action) {
     case CONTEXT_UPDATE : {
       state = Object.assign({}, initialState, state, {
         [action.context]:
-          (typeof action.data == 'string' ? action.data : {
+          (typeof action.data == 'object' ? {
             ...state[action.context],
             ...action.data
-          })
+          } : action.data)
       });
       return state;
     }
